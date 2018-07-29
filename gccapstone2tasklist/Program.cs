260e9866c27bc23b3232095c795dbefa8885c7ca
chris <butchchr@gmail.com>
2018-07-29 17:10:04 -0400

    first commit, task menu set up

--- gccapstone2tasklist/Program.cs
+++ gccapstone2tasklist/Program.cs
@@ -10,6 +10,38 @@ namespace gccapstone2tasklist
     {
         static void Main(string[] args)
         {
+            bool y = true;
+            while (y)
+            {
+
+
+                Console.WriteLine("Welcome to the Task Manager!\n\t1. List tasks\n\t 2. Add task\n\t3. Delete task\n\t4. Mark task complete\n\t5. Quit\n What would you like to do?");
+                string dataFromUser = Console.ReadLine();
+
+                int userNum;
+                bool num1 = int.TryParse(dataFromUser, out userNum);
+
+                if (!num1)
+                {
+                    Console.WriteLine("You did not enter a valid number");
+                }
+
+                //do the magic with the tasks
+
+                //Continue?
+                bool invalid = true;
+                while (invalid)
+                {
+                    Console.WriteLine("Continue? (y/n):");
+                    ConsoleKeyInfo pressed = Console.ReadKey();
+                    Console.WriteLine();
+                    bool isY = pressed.Key == ConsoleKey.Y;
+                    bool isN = pressed.Key == ConsoleKey.N;
+
+                    invalid = !isY && !isN;
+                    y = isY;
+                }
+            }
         }
     }
 }
